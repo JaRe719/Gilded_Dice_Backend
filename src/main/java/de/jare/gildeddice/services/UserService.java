f390c65c096020c844a0e18d63c64450739e946b
René Behrens <rene.behrens@aol.com>
2024-12-03 19:56:01 +0100

    Update UserService.java

--- src/main/java/de/jare/gildeddice/services/UserService.java
+++ src/main/java/de/jare/gildeddice/services/UserService.java
@@ -3,6 +3,7 @@ package de.jare.gildeddice.services;
 import de.jare.gildeddice.dtos.UserRegisterRequestDTO;
 import de.jare.gildeddice.entities.users.Profile;
 import de.jare.gildeddice.entities.users.User;
+import de.jare.gildeddice.repositories.ProfileRepository;
 import de.jare.gildeddice.repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,16 +13,29 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private UserRepository userRepository;
+    private ProfileRepository profileRepository;
     private PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, ProfileRepository profileRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.profileRepository = profileRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Transactional
     public void newUserRegister(UserRegisterRequestDTO dto) {
+        if (userRepository.findByEmail(dto.email()).isPresent()) {
+            throw new IllegalArgumentException("Email already exists");
+        }
         User user = new User();
         Profile profile = new Profile();
         user.setEmail(dto.email());
         user.setPassword(passwordEncoder.encode(dto.password()));
         profile.setUsername(dto.username());
+
+        profile = profileRepository.save(profile);
         user.setProfile(profile);
+        userRepository.save(user);
     }
 
 

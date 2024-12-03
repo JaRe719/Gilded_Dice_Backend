8c03d2a86d67f5743877a833d1bdfcd6381b630f
René Behrens <rene.behrens@aol.com>
2024-12-03 12:26:35 +0100

    create Project

---
+++ src/main/java/de/jare/gildeddice/GildedDiceApplication.java
@@ -0,0 +1,13 @@
+package de.jare.gildeddice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GildedDiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GildedDiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/jare/gildeddice/GildedDiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.jare.gildeddice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GildedDiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

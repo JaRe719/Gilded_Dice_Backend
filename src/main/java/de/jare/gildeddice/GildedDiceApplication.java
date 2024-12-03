1528bb261f7ec0055b5f62b80bd269c20a6b5035
René Behrens <rene.behrens@aol.com>
2024-12-03 18:52:20 +0100

    create Security/app Config

--- src/main/java/de/jare/gildeddice/GildedDiceApplication.java
+++ src/main/java/de/jare/gildeddice/GildedDiceApplication.java
@@ -1,9 +1,12 @@
 package de.jare.gildeddice;
 
+import de.jare.gildeddice.configurations.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class GildedDiceApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/de/jare/gildeddice/configurations/AppConfig.java
@@ -0,0 +1,32 @@
+package de.jare.gildeddice.configurations;
+
+import de.jare.gildeddice.entities.users.User;
+import de.jare.gildeddice.repositories.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class AppConfig {
+    private UserRepository userRepository;
+
+    public AppConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return username -> {
+            User user = userRepository.findByEmail(username).orElseThrow(()->new UsernameNotFoundException("User not found"));
+            return user;
+        };
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}

f7008ea8be953fb5ff53a422ead65379fc98e8b3
René Behrens <rene.behrens@aol.com>
2024-12-05 01:10:52 +0100

    add phase variable to story

--- src/main/java/de/jare/gildeddice/repositories/StoryRepository.java
+++ src/main/java/de/jare/gildeddice/repositories/StoryRepository.java
@@ -4,4 +4,6 @@ import de.jare.gildeddice.entities.games.storys.Story;
 import org.springframework.data.repository.CrudRepository;
 
 public interface StoryRepository extends CrudRepository<Story, Long> {
+    Story findByPhase(int phase);
+
 }

ac44978f52bd47da7c2cd607d2dd47b81ebfa0b9
René Behrens <rene.behrens@aol.com>
2024-12-06 12:17:52 +0100

    add new variable in choice

--- src/main/java/de/jare/gildeddice/entities/games/storys/Choice.java
+++ src/main/java/de/jare/gildeddice/entities/games/storys/Choice.java
@@ -16,16 +16,33 @@ public class Choice {
     private String title;
     private Skill skill;
 
-    @Column(length = 1024)
     private int minDiceValue;
+
     @Column(length = 1024)
     private String startMessage;
+
     @Column(length = 1024)
     private String winMessage;
+    private int winIncomeValue;
+    private int winOutcomeValue;
+    private int winInvestmentPercent;
+
     @Column(length = 1024)
     private String loseMessage;
+    private int loseIncomeValue;
+    private int loseOutcomeValue;
+    private int loseInvestmentPercent;
+
     @Column(length = 1024)
     private String critMessage;
+    private int critIncomeValue;
+    private int critOutcomeValue;
+    private int critInvestmentPercent;
+
+    private int newStressValue;
+    private int newSatisfactionValue;
+    private int newHealthValue;
+
 
     @ManyToOne
     private Npc npc;

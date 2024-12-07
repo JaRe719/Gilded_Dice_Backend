a314cf61bd19433f83ab5f1455f5515c9a59b01c
René Behrens <rene.behrens@aol.com>
2024-12-07 18:44:07 +0100

    Create PlusStory.java

---
+++ src/main/java/de/jare/gildeddice/entities/games/storys/PlusStory.java
@@ -0,0 +1,29 @@
+package de.jare.gildeddice.entities.games.storys;
+
+import de.jare.gildeddice.entities.enums.Category;
+import de.jare.gildeddice.entities.games.choices.Choice;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+public class PlusStory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private Category category;
+    private String title;
+    private String prompt;
+    private boolean skippable;
+
+    @OneToOne(cascade = {CascadeType.ALL})
+    private Requirement requirement;
+    @OneToMany
+    private List<Choice> choices = new ArrayList<>();
+}
+

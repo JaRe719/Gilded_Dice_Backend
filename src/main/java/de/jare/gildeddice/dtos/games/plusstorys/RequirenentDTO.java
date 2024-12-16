334f1e62b25d8551c09af0198d399b22e39f4178
René Behrens <114138012+ReBehrens@users.noreply.github.com>
2024-12-16 11:26:06 +0100

    added more choices Variables

--- src/main/java/de/jare/gildeddice/dtos/games/plusstorys/RequirenentDTO.java
+++ src/main/java/de/jare/gildeddice/dtos/games/plusstorys/RequirenentDTO.java
@@ -1,13 +1,23 @@
 package de.jare.gildeddice.dtos.games.plusstorys;
 
 public record RequirenentDTO(
+
         Boolean hasStudie,
+        Boolean hasScholarship,
         Boolean hasApprenticeship,
+        Boolean hasSecondJob,
         Boolean hasJob,
-        Boolean hasProperty,
+
+        Boolean insurance,
+
+        Boolean hasHomeByParents,
+        Boolean hasSharedApartment,
         Boolean hasRentedApartment,
+
+        Boolean hasProperty,
         Boolean hasCar,
         Boolean hasDriverLicense,
+
         Boolean hasInvested,
         Integer stressStatusLvl,
         Integer satisfactionStatusLvl,
